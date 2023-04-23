44a336a3ca1d342d763d464e9579bf1d947314bd
Jeroen Voogd <jpvoogd@icloud.com>
2023-04-23 14:16:46 +0200

    Initial Commit

---
+++ SleepMusic/SleepMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepMusicApp.swift
+//  SleepMusic
+//
+//  Created by Jeroen Voogd on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SleepMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
